442dba09135f8cf7052e599479f9559bd226808b
mscifu <mscifu@gmail.com>
2021-05-17 20:35:40 -0300

    added start screen

--- Videos.gd
+++ Videos.gd
@@ -6,38 +6,46 @@ var files_arr
 var files_idx = 0
 
 onready var startscreen_timer = get_node("startscreen_timer")
+onready var startscreen = get_node("start_screen")
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	video_stream = VideoStreamWebm.new()
 	video_player = get_node("videoPlayer")
 	files_arr = list_files_in_directory('fr_videos/')
-	play_next_video()
-	
-	
+	hide_videos()
+	# testing
+	show_videos()
+
+
 func hide_videos():
 	video_player.stop()
 	video_player.hide()
-	#startscreen.hide_startscreen()
-	
+	startscreen.hide()
+	startscreen_timer.stop()
+
+
 func show_videos():
-	video_player.show()
-	video_player.play()
-	#startscreen.show_startscreen()
-		
-		
-# TODO: armar startscreen, con la animacion de la manito
-# y el control que salta cuando apreta el control		
+	play_next_video()
+
 
 func play_next_video():
 	if(files_idx >=  files_arr.size()):
 		files_idx = 0
-			
+	
+	video_player.show()
+	startscreen.hide()
+
 	video_stream.set_file(files_arr[files_idx])
 	video_player.stream = video_stream
 	video_player.play()
 	files_idx += 1
+
+
+func show_startscreen():
+	video_player.hide()
+	startscreen.show()
+	startscreen_timer.start()
 
 
 func list_files_in_directory(path):
@@ -58,7 +66,8 @@ func list_files_in_directory(path):
 
 
 func _on_videoPlayer_finished():
-	# poner el instructico (otro movie)
-	# activar el timer 
-	# cuando termina el timer playear el siguiente
+	show_startscreen()
+
+
+func _on_startscreen_timer_timeout():
 	play_next_video()
